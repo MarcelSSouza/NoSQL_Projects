1e29ef8ec8ec308bdb5265613048df1721713ad8
MarcelSSouza <marcelaraujosantossouza19@ua.pt>
2022-10-20 12:17:24 +0100

    Finalizaçao do 3-1

---
+++ Lab3/users/src/main/java/com/users/users/User.java
@@ -0,0 +1,87 @@
+package com.users.users;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "persons")
+public class User {
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    //Getters and setters
+
+    public User() {
+    }
+
+    public User(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User id(long id) {
+        setId(id);
+        return this;
+    }
+
+    public User name(String name) {
+        setName(name);
+        return this;
+    }
+
+    public User email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", email='" + getEmail() + "'" +
+            "}";
+    }
+
+
+
+}
+
+
---
+++ Lab3/users/src/main/java/com/users/users/UserRepository.java
@@ -0,0 +1,6 @@
+package com.users.users;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
